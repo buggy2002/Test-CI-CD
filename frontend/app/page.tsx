6016dad4dba947704dc1c054dc720f1f950408c1
buggy2002 <thanakritskyheart@gmail.com>
2026-04-21 09:51:57 +0700

    test pipe line ci cd

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -22,9 +22,14 @@ export default async function Home() {
         <p className="text-xl font-medium text-slate-300">API Status:</p>
         <p className="text-3xl font-bold text-green-400 mt-2">{data.message}</p>
       </div>
-      <button className="bg-blue-500 mt-4 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-        Click Me
-      </button>
+      <div className="mt-4 flex gap-4">
+        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+          Click Me
+        </button>
+        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+          Hover ME
+        </button>
+      </div>
     </div>
   );
 }
